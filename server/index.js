a7ec257ec64945400511548bd15377982cc641ba
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-24 13:48:55 -0400

    Add GET handler for root route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
-const server = require('./src/server');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const server = express();
+
+server.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Success' });
+});
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(cors());
 
 const port = process.env.PORT || 5000;
 
